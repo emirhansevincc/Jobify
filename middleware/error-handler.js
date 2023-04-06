606481b450e51f842de5add6d8cefe2408a52600
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-04-06 21:27:39 +0300

    Error-1 | Missing field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,12 +7,55 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         msg: 'Something went wrong, please try again later!'
     };
 
-    res.status(defaultError.statusCode).json({
-        msg: defaultError.msg
-    });
+    if (err.name === 'ValidationError') {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST;
+        defaultError.msg = Object.values(err.errors).map((val) => val.message).join(',');
+    }
+
+    res.status(defaultError.statusCode).json({ msg: defaultError.msg })
+    res.status(defaultError.statusCode).json({msg: err});
+
 } 
 
 export default errorHandlerMiddleware;
 
 
 
+
+
+
+
+
+/////////////////////////////// Error Object ///////////////////////////////
+
+// {
+//     "msg": {
+//         "errors": {
+//             "password": {
+//                 "name": "ValidatorError",
+//                 "message": "Password is required",
+//                 "properties": {
+//                     "message": "Password is required",
+//                     "type": "required",
+//                     "path": "password"
+//                 },
+//                 "kind": "required",
+//                 "path": "password"
+//             },
+//             "name": {
+//                 "name": "ValidatorError",
+//                 "message": "Name is required",
+//                 "properties": {
+//                     "message": "Name is required",
+//                     "type": "required",
+//                     "path": "name"
+//                 },
+//                 "kind": "required",
+//                 "path": "name"
+//             }
+//         },
+//         "_message": "User validation failed",
+//         "name": "ValidationError",
+//         "message": "User validation failed: password: Password is required, name: Name is required"
+//     }
+// }
