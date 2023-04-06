87b08150a05db7c49ee87ce040a1a769970f6b18
Emirhan Sevinc <emirhansvncc@gmail.com>
2023-04-06 22:14:34 +0300

    Error-4 | CustomApiError

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 
+class CustomApiError extends Error {
+    constructor(message, statusCode) {
+        super(message); // it is coming from the Error object (message)
+        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
 const register = async (req, res) => {
     const { name, email, password } = req.body;
 
     if(!name || !email || !password) {
-        throw new Error('Please enter all fields');
+        throw new CustomApiError('Please enter all fields');
     }
 
     const user = await User.create({ name, email, password });
